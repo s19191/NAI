93bdd9241604c271cc87f3eeabf866e656ce187b
s19191 <s19191@pjwstk.edu.pl>
2020-05-13 01:27:08 +0200

    prawie nic nie ma

--- projekt05/src/Main.java
+++ projekt05/src/Main.java
@@ -3,23 +3,46 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
+        List<Iris> training = load("iris_training.txt");
+        List<Iris> test = load("iris_test.txt");
+        Map<String, Integer> irisTypes = createPropablility(training);
+    }
+
+    public static Map<String, Integer> createPropablility(List<Iris> irises) {
+        Map<String, Integer> irisesMap = new HashMap<>();
+        for (int i = 0; i < irises.size(); i++) {
+            if (irisesMap.containsKey(irises.get(i).name)) {
+                Integer tmp = irisesMap.get(irises.get(i).name);
+                irisesMap.remove(irises.get(i).name);
+                irisesMap.put(irises.get(i).name, tmp++);
+            } else {
+                irisesMap.put(irises.get(i).name, 1);
+            }
+        }
+
+        return irisesMap;
+    }
+    public static List<Iris> load (String path) {
         List<String> lines = new ArrayList<>();
         try {
-            lines = Files.readAllLines(new File("iris_training.txt").toPath(), StandardCharsets.UTF_8);
+            lines = Files.readAllLines(new File(path).toPath(), StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        List<Iris> training = new ArrayList<>();
+        List<Iris> irises = new ArrayList<>();
         int numberOfAttributes = lines.get(0).trim().split("\\s+").length -1;
         for (int i = 0; i < lines.size(); i++) {
             if (lines.get(i).trim().split("\\s+").length == numberOfAttributes + 1) {
-                training.add(new Iris(lines.get(i).trim().split("\\s+"), numberOfAttributes));
+                irises.add(new Iris(lines.get(i).trim().split("\\s+"), numberOfAttributes));
             }
         }
+        return irises;
     }
 }
